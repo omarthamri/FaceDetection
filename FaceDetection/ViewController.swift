dd7774fa1a86d94f35fe4744f78040d98a01c725
MACBOOK PRO RETINA <macbookproretina@MacBook-Pro-de-MACBOOK-2.local>
2019-06-20 20:42:27 +0100

    #0001 change background color

--- FaceDetection/ViewController.swift
+++ FaceDetection/ViewController.swift
@@ -14,12 +14,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.init(white: 0.9, alpha: 1)
+        
         guard let image = UIImage(named: "suits") else { return }
         let imageView = UIImageView(image: image)
         imageView.contentMode = .scaleAspectFit
          let scaledHeight = view.frame.width / image.size.width * image.size.height
         imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: scaledHeight)
-        imageView.backgroundColor = .blue
         view.addSubview(imageView)
         
         let request =  VNDetectFaceRectanglesRequest { (req, error) in
